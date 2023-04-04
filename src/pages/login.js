aa2ea918a0d69b4a474b4f0ee0801501471a2b42
Joy <kamlomo.js@gmail.com>
2023-04-04 18:34:32 +0200

    fixed login routing error

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,29 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-function login() {
-    const router = useRouter()
+export default function login() {
+  const router = useRouter();
   return (
     <div>
-        {/* add login form */}
-        <h1>Login to your account</h1>
-        <button onClick={() => router.push("/")}>Login</button>
+      <h1>Login to your account</h1>
+//         <button onClick={() => router.push("/")}>Login</button>
     </div>
   )
 }
 
-export default login
+
+// import { useRouter } from 'next/router'
+// import React from 'react'
+
+// function login() {
+//     const router = useRouter()
+//   return (
+//     <div>
+//         {/* add login form */}
+//         <h1>Login to your account</h1>
+//         <button onClick={() => router.push("/")}>Login</button>
+//     </div>
+//   )
+// }
+
+// export default login
