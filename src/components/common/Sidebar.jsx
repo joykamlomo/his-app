322e43aae37e12eb5a8d6124b0a4b66fba48c75a
Joy <kamlomo.js@gmail.com>
2023-04-05 20:58:03 +0200

    Sidebar redone

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { UserIcon, UsersIcon } from '@heroicons/react/24/outline' 
+import { BookmarkSlashIcon, BriefcaseIcon, CurrencyPoundIcon, DocumentCheckIcon, DocumentDuplicateIcon, FaceSmileIcon, UserIcon, UsersIcon } from '@heroicons/react/24/outline' 
 import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <aside className="bg-gray-50 w-64 p-4 shadow text-gray-900 tracking-wide">
-      <h2 className="text-4xl mb-4 font-semibold">WEMUS</h2>
-      {/* <p className='text-xs mb-4 border-b py-3 text-indigo-600'>Wella Medical Aid Society</p> */}
-      <div>
-      <h6 className="text-gray-400 text-xs font-medium uppercase">menu</h6>
-      <ul className='mt-3 px-2 text-gray-900 font-medium text-sm tracking-relaxed'>
-        <li className='py-3 px-2 mb-2 flex items-center space-x-3 rounded-full hover:bg-gray-900 hover:text-white'>
-          <UserIcon className='w-5'/>
+    <aside className="w-72 p-4 shadow text-gray-900 tracking-wide">
+        <h2 className="text-4xl font-title tracking-wide">WEMUS</h2>
+        <p className='text-xs text-indigo-600 tracking-tight mb-4'>Wella Medical Aid Society</p>
+        <hr className=''/>
+      <div className='mt-2'>
+      <h6 className="text-gray-400 text-xs uppercase">menu</h6>
+      <ul className='mt-1 text-gray-700 text-sm tracking-relaxed border-b'>
+        <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
+          <BookmarkSlashIcon className='w-5 mr-1'/>
           <Link href={''}>Dashboard</Link>
         </li>
-        <li className='py-3 px-2 mb-2 flex items-center space-x-3 rounded-full hover:bg-gray-900 hover:text-white'>
-          <UserIcon className='w-5'/>
-          <Link href={''}>Dashboard</Link>
+        <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
+          <BriefcaseIcon className='w-5 mr-1'/>
+          <Link href={''}>Policies</Link>
+        </li>
+        <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
+          <DocumentCheckIcon className='w-5 mr-1'/>
+          <Link href={''}>Claims</Link>
+        </li>
+        <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
+          <CurrencyPoundIcon className='w-5 mr-1'/>
+          <Link href={''}>Payments</Link>
+        </li>
+        <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
+          <DocumentDuplicateIcon className='w-5 mr-1'/>
+          <Link href={''}>Reports & Analytics</Link>
+        </li>
+        <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
+          <UsersIcon className='w-5 mr-1'/>
+          <Link href={''}>CASA</Link>
         </li>
         
       </ul>
