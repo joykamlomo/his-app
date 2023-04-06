00974fc87fdc0791bf01c181dc61f767380a2840
victor <kamlomovictor@gmail.com>
2023-04-06 03:30:11 +0200

    Login fixed

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-export default function login() {
+export default function Login() {
   const router = useRouter()
   return (
     <div>
