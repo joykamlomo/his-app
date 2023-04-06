0104b6113a4ea088886c4ca6f7e4f78fc7aaf121
Joy <kamlomo.js@gmail.com>
2023-04-06 16:28:01 +0200

    login update

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,7 +18,7 @@ function Login() {
               alt="Your Company"
             />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Health Insurance Systen
+              Health Insurance System
             </h2>
           </div>
       </div>
