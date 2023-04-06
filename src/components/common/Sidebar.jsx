fbc3b4fcbe2e0c8a65a8eb9e016cd3d9e960afa8
victor <kamlomovictor@gmail.com>
2023-04-06 03:47:48 +0200

    Created module folders

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -6,9 +6,9 @@ const Sidebar = () => {
   return (
     <aside className="w-72 p-4 shadow text-gray-900 tracking-wide">
         <h2 className="text-4xl font-title tracking-wide">WEMUS</h2>
-        <p className='text-xs text-indigo-600 tracking-tight mb-4'>Wella Medical Aid Society</p>
-        <hr className=''/>
-      <div className='mt-2'>
+        <p className='text-xs text-indigo-600 tracking-tight my-1 mx-1'>Wella Medical Aid Society</p>
+        <hr className='mt-4'/>
+      <div className='mt-4'>
       <h6 className="text-gray-400 text-xs uppercase">menu</h6>
       <ul className='mt-1 text-gray-700 text-sm tracking-relaxed border-b'>
         <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
@@ -33,7 +33,7 @@ const Sidebar = () => {
         </li>
         <li className='py-2 px-2 mb-2 flex items-center rounded w-full hover:bg-gray-900 hover:text-white'>
           <UsersIcon className='w-5 mr-1'/>
-          <Link href={''}>CASA</Link>
+          <Link href={''}>Customers</Link>
         </li>
         
       </ul>
---
+++ src/pages/reportsAndAnalytics/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Reporting() {
+  return (
+    <div>Reporting</div>
+  )
+}
+
+export default Reporting
